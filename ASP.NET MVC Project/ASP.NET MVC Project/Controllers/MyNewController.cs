c562eb1fb165ca52bb240a17e32262d500ca72f8
mayankpal46 <mayankpal4629@gmail.com>
2024-11-06 11:04:23 +0530

    Lecture no:-05 Controller in MVC

---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/MyNewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class MyNewController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/ParameterController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class ParameterController : Controller
+    {
+        public string ID(int no)
+        {
+            return "String Type Method";
+        }
+
+        //Parameters which are mandatory
+        public string Newmethod(int num , string name)
+        {
+            return "My Age is " + num + " And My name is " + name;
+        }
+
+        //Nullable parameter
+        public string nparameter(int no , int ? val=null)
+        {
+            return "Id is " + no;
+        }
+    }
+}
