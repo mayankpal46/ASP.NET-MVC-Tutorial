4b0dd9d8aff9ff677d8ce238b4dd22def0fcdc34
mayankpal46 <mayankpal4629@gmail.com>
2024-11-07 16:18:02 +0530

    Lecture :- 17 (  HttpVerbs )

---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/HttpVerbsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class HttpVerbsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(int id)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Pmethod(int id)
+        {
+            return View();
+        }
+
+        [HttpPut]
+        public ActionResult updatemethod(int id)
+        {
+            return View();
+        }
+
+        [HttpDelete]
+        public ActionResult deletemethod(int id)
+        {
+            return View();
+        }
+
+    }
+}
