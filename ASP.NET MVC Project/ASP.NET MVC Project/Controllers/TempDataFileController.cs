70382d34d4215768d6073de8ad50798e0dcd7a13
mayankpal46 <mayankpal4629@gmail.com>
2024-11-07 15:40:26 +0530

    Lecture :- 16 (  TempData )

---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/TempDataFileController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class TempDataFileController : Controller
+    {
+        
+        public ActionResult M1()
+        {
+            TempData["nokey"] = "First Method";
+            TempData.Keep();
+            return View();
+        }
+
+        public ActionResult M2()
+        {
+            ViewBag.m2val = TempData["nokey"];
+            TempData.Keep("nokey");
+            return View();
+        }
+
+        public ActionResult M3()
+        {
+            ViewBag.m3val = TempData["key1"];
+            return View();
+        }
+    }
+}
