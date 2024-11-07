4a6f26ed3b76759f44c545e6b953e0c86e0e136e
mayankpal46 <mayankpal4629@gmail.com>
2024-11-07 09:48:51 +0530

    Lecture :- 12 (  Strongly Typed Html Helpers )

---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/StronglyTypeHelpersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ASP.NET_MVC_Project.Models;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class StronglyTypeHelpersController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            StronglyType obj = new StronglyType();
+            obj.No = 100;
+            obj.Address = "XYZ";
+            obj.Name = "Rudra";
+            return View(obj);
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(string name = null)
+        {
+            return View();
+        }
+    }
+}
