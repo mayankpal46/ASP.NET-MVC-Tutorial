a9060e68a4e01672de014ec3fd8e8a8f7ec69735
mayankpal46 <mayankpal4629@gmail.com>
2024-11-07 14:26:19 +0530

    Lecture :- 15 (  ViewBag & ViewData )

---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/ViewBagAndViewDataController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class ViewBagAndViewDataController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            ViewBag.myname = "Rudra";
+            ViewBag.Revenue = 1000000;
+            ViewData["key1"] = "Omkara";
+            ViewData["Listkey"] =  new List<string>() { "name1","name2","name3", "name4" };
+            return View();
+        }
+    }
+}
