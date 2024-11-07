9fb11a495bb6e36fa75d250501c61ec0b89f4a06
mayankpal46 <mayankpal4629@gmail.com>
2024-11-07 11:07:01 +0530

    Lecture :- 13 (  Templated Html Helpers )

---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/TemplatedHtmlHelpersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP.NET_MVC_Project.Models;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class TemplatedHtmlHelpersController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            TemplatedHtmlHelpers obj = new TemplatedHtmlHelpers();
+            obj.Name = "Omkara";
+            obj.num = 1000000;
+            return View(obj);
+        }
+
+
+
+        public ActionResult Edit()
+        {
+            TemplatedHtmlHelpers obj = new TemplatedHtmlHelpers();
+            obj.Name = "Omkara";
+            obj.num = 1000000;
+            return View(obj);
+            
+        }
+    }
+}
---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Models/TemplatedHtmlHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET_MVC_Project.Models
+{
+    public class TemplatedHtmlHelpers
+    {
+        
+        public int num { get; set; }
+
+        /*This will display in view*/
+        [Display(Name = "Enter Your Name")]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime newdate { get; set; }   
+    }
+}
