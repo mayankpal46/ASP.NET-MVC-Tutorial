97ea797b9764cbb92f03f09a38c7da8d9d04aec1
mayankpal46 <mayankpal4629@gmail.com>
2024-11-07 08:41:42 +0530

    Lecture :- 11 (  Standard Html Helpers )

---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/StandardHelperController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class StandardHelperController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About(string name = null)
+        {
+            return View();
+        }
+    }
+}
