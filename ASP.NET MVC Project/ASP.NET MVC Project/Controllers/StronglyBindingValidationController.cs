dbb2c5650cc2fa17149a5e1d81b5938d660be467
mayankpal46 <mayankpal4629@gmail.com>
2024-11-16 19:59:04 +0530

    Lecture - 23 ( Validation with strongly binding )

---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/StronglyBindingValidationController.cs
@@ -0,0 +1,24 @@
+﻿using ASP.NET_MVC_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class StronglyBindingValidationController : Controller
+    {
+        // GET: StronglyBindingValidation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult stronglybinding(Students data)
+        {
+            return View();
+        }
+    }
+}
