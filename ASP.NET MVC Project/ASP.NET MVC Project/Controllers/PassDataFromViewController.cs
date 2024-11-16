1374fe896fc2109af528367658a442d85c1ef3a9
mayankpal46 <mayankpal4629@gmail.com>
2024-11-16 19:00:04 +0530

    Lecture - 22 ( Pass Data from View to Controller )

--- ASP.NET MVC Project/ASP.NET MVC Project/Controllers/PassDataFromViewController.cs
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/PassDataFromViewController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ASP.NET_MVC_Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASP.NET_MVC_Project.Models;
 
 namespace ASP.NET_MVC_Project.Controllers
 {
@@ -14,10 +16,41 @@ namespace ASP.NET_MVC_Project.Controllers
             return View();
         }
 
+        /*Parameters name should not be different which u have passed in a view*/
+
         [HttpPost]
-        public ActionResult PostUsingParameters()
+        public string PostUsingParameters(string FirstName, string LastName)
         {
-            return View();
+            return "Your first name is : " + FirstName + " And last name is : " + LastName;
+        }
+
+        [HttpPost]
+        public string PostUsingRequest()
+        {
+            //key need to be same which is in view
+            string firstname = Request["FirstName"];
+            string lastname = Request["LastName"];
+            return "Your first name is : " + firstname + " And last name is : " + lastname;
+
+        }
+
+        [HttpPost]
+        public string PostUsingFormCollection(FormCollection form)
+        {
+            //key need to be same which is in view
+            string firstname = form["FirstName"];
+            string lastname = form["LastName"];
+            return "Data using Form Collection";
+
+        }
+
+
+        [HttpPost]
+        public string PostUsingStronglyBinding(Students data)
+        {
+            
+            return "Data using Strongly Type Binding : " + data.Name;
+
         }
     }
 }
