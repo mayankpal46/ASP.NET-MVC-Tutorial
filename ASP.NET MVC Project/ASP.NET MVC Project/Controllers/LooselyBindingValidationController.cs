df174d485085da5a34cca00405e02b31c4294264
mayankpal46 <mayankpal4629@gmail.com>
2024-11-16 19:44:06 +0530

    Lecture no - 22 ( Validation in Loosely Binding

---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/LooselyBindingValidationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP.NET_MVC_Project.Models;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class LooselyBindingValidationController : Controller
+    {
+        // GET: LooselyBindingValidation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult submitdata(Students data)
+        {
+            if(ModelState.IsValid)
+            {
+                return View();
+            }
+            return View("Index");
+        }
+    }
+}
--- ASP.NET MVC Project/ASP.NET MVC Project/Models/Students.cs
+++ ASP.NET MVC Project/ASP.NET MVC Project/Models/Students.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASP.NET_MVC_Project.Models
 {
     public class Students
     {
+        public int Age { get; set; }
         public int Id { get; set; }
+
         public string Name { get; set; }
+
+        [Required(ErrorMessage ="Email Address sahi nahi hai")]
+        [EmailAddress]
+        public string EmailId { get; set; }
+        public string LastName { get; set; }
+
         public string Address { get; set; }
     }
 }
