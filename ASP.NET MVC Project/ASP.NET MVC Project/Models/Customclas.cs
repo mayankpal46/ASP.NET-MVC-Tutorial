547e51b359291ddfc6fb9478397275df0e2cb164
mayankpal46 <mayankpal4629@gmail.com>
2024-11-17 09:43:32 +0530

    Lecture no 24 ( Custom Validation )

---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Models/Customclas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET_MVC_Project.Models
+{
+    public class Customclas
+    {
+        [Required]
+        public string name { get; set; }
+        
+        [newvalidation(ErrorMessage ="Rudra is required")]
+        public string address { get; set; }
+    }
+}
---
+++ ASP.NET MVC Project/ASP.NET MVC Project/newvalidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace ASP.NET_MVC_Project
+{
+    public class newvalidation :ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string message = value.ToString();
+                if (message.Contains("Rudra"))
+                {
+                    return ValidationResult.Success;
+                }
+               
+            }
+
+            return new ValidationResult("Some Error is there");
+        }
+    }
+}
