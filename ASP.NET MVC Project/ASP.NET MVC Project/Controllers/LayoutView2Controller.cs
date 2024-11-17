3d33f3d6c8e7f5542ed1463d799b2579a2f12ef4
mayankpal46 <mayankpal4629@gmail.com>
2024-11-17 10:58:38 +0530

    Lecture - 26 ( Layout in MVC )

---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/LayoutView2Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class LayoutView2Controller : Controller
+    {
+        // GET: LayoutView2
+        [Route("GetList")]
+        public ActionResult GetList()
+        {
+            return View();
+        }
+
+        [Route("AddList")]
+        public ActionResult AddList()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/LayoutViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class LayoutViewController : Controller
+    {
+        // GET: LayoutView
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
