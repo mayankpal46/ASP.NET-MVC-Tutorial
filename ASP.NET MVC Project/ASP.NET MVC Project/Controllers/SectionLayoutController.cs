978138ffefe8665a23c646fbec409fd199a1302f
mayankpal46 <mayankpal4629@gmail.com>
2024-11-17 11:58:56 +0530

    Lecture - 28 ( Section in Layout )

---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/SectionLayoutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class SectionLayoutController : Controller
+    {
+        // GET: SectionLayout
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult m1()
+        {
+            return View();
+        }
+    }
+}
